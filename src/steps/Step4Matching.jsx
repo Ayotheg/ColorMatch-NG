9843f0f5585f37d31f6da0a4e71d36b546e43576
Ayotheg <haywhyshotz@gmail.com>
2026-04-27 02:30:08 +0800

    Cards design

--- src/steps/Step4Matching.jsx
+++ src/steps/Step4Matching.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavbar } from "../context/NavbarContext";
 import { useNavigate } from "react-router-dom";
-import { ArrowLeft, Share, Circle } from "lucide-react";
+import { ArrowLeft, Share, Circle, Sparkle,LayoutGrid , Sparkles, Waves} from "lucide-react";
 import Button from "../components/Button";
 
 export default function Step4Matching() {
@@ -44,39 +44,39 @@ export default function Step4Matching() {
       <div className="button-list bg-white px-5 py-2  leading-6 font-button rounded-4xl text-left ">
         <div className="select block align-left relative bottom-2">
            <span className="text-primary relative left-72 top-5"><Circle /></span>
-              <h1 className="font-bold mb-1 text-xl">Emulsion </h1>
-                <h2 className="font-medium font-heading">Standard/Matte Finish</h2>
+              <h1 className="font-bold mb-1 text-xl flex gap-2"> <span className="text-amber-900"><Waves /></span>Emulsion </h1>
+                <h2 className="font-medium font-heading text-amber-900">Standard/Matte Finish</h2>
                    <p className="font-body text-gray-500 text-sm mb-2 mt-1">Perfect for living rooms and bedrooms. <br /> Hides walls imperfection beautifully</p>
                     </div>
                        </div>
                         </div>
-      <div className="card-2 py-5 px-2">
+      <div className="card-2 py-5 relative left-1 pl-2">
       <div className="button-list bg-gray-200 px-5 py-2  leading-6 font-button rounded-4xl text-left ">
         <div className="select block align-left relative bottom-2">
            <span className="text-primary relative left-72 top-5"><Circle /></span>
-              <h1 className="font-bold mb-1 text-xl">Emulsion </h1>
-                <h2 className="font-medium font-heading">Standard/Matte Finish</h2>
-                   <p className="font-body text-gray-500 text-sm mb-2 mt-1">Perfect for living rooms and bedrooms. <br /> Hides walls imperfection beautifully</p>
+              <h1 className="font-bold mb-1 text-xl flex gap-2"><span className="text-emerald-700"><Sparkles /></span>Satin </h1>
+                <h2 className="font-medium font-heading text-emerald-700">Smooth/Wipeable</h2>
+                   <p className="font-body text-gray-500 text-sm mb-2 mt-1">Durability with a soft glow. Great for kitchens  <br />and high traffic corridors</p>
                 </div>
                    </div>
                      </div>
-      <div className="card-3 py-5">
+      <div className="card-3 py-5 pr-2">
       <div className="button-list bg-white px-5 py-2  leading-6 font-button rounded-4xl text-left ">
         <div className="select block align-left relative bottom-2">
            <span className="text-primary relative left-72 top-5"><Circle /></span>
-              <h1 className="font-bold mb-1 text-xl">Emulsion </h1>
-                <h2 className="font-medium font-heading">Standard/Matte Finish</h2>
-                   <p className="font-body text-gray-500 text-sm mb-2 mt-1">Perfect for living rooms and bedrooms. <br /> Hides walls imperfection beautifully</p>
-                    </div>
+              <h1 className="font-bold mb-1 text-xl flex gap-2"><span className="text-stone-800"><LayoutGrid /></span>Texcoat </h1>
+                <h2 className="font-medium font-heading text-stone-800">Textured Outdoor</h2>
+                   <p className="font-body text-gray-500 text-sm mb-2 mt-1">Rugged and weather-resistant. Ideal for Nigerian exterior facades</p>
+                    </div> 
                        </div>
                           </div>
-              <div className="card-4 py-5">
-              <div className="button-list bg-white px-5 py-2  leading-6 font-button rounded-4xl text-left ">
+              <div className="card-4 py-5 pl-2" >
+              <div className="button-list bg-gray-200 px-5 py-2  leading-6 font-button rounded-4xl text-left ">
                 <div className="select block align-left relative bottom-2">
                   <span className="text-primary relative left-72 top-5"><Circle /></span>
-                      <h1 className="font-bold mb-1 text-xl">Emulsion </h1>
-                        <h2 className="font-medium font-heading">Standard/Matte Finish</h2>
-                          <p className="font-body text-gray-500 text-sm mb-2 mt-1">Perfect for living rooms and bedrooms. <br /> Hides walls imperfection beautifully</p>
+                      <h1 className="font-bold mb-1 text-xl flex gap-2"><span className=" text-blue-600"><Sparkle /></span>Gloss </h1>
+                        <h2 className="font-medium font-heading text-blue-600">Shiny / Bold</h2>
+                          <p className="font-body text-gray-500 text-sm mb-2 mt-1">High-reflectivity finish. Best for wood, metal, and feature details.</p>
                            </div>
                                </div>
                                   </div>
