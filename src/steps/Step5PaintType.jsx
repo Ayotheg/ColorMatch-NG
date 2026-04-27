5842f37a47da8d06d09df82a675b50208c166efa
Ayotheg <haywhyshotz@gmail.com>
2026-04-27 14:02:51 +0100

    Added navbar

--- src/steps/Step5PaintType.jsx
+++ src/steps/Step5PaintType.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavbar } from "../context/NavbarContext";
 import { useNavigate } from "react-router-dom";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Blinds } from "lucide-react";
 import Button from "../components/Button";
 
 export default function Step5PaintType() {
@@ -39,9 +39,18 @@ export default function Step5PaintType() {
   return (
     <div className="flex-1 flex flex-col relative px-6 pt-4 pb-32">
       <div className="flex-1">
-        <h1 className="text-2xl font-bold">Step 5: Paint Type</h1>
+        <h1 className="text-4xl font-bold font-body">Do you have <br /> <span className="text-primary">something</span> to <br />match?</h1>
+      <p className="text-gray-500 pt-2">Select any element in your space that we should harmonize with</p>
       </div>
       
+
+      <div className="choice-card py-8 grid grid-cols-2">
+        <div className="card block bg-gray-200 py-5 px-5 w-40 h-22 rounded-sm leading-8"> <span className="text-primary"><Blinds /></span>  Curtain</div>
+        <div className="card block bg-gray-200 py-5 px-5 w-40 h-22 rounded-sm leading-8"> <span className="text-primary"><Blinds /></span>  Curtain</div>
+        <div className="card block bg-gray-200 py-5 px-5 w-40 h-22 rounded-sm leading-8"> <span className="text-primary"><Blinds /></span>  Curtain</div>
+        <div className="card block bg-gray-200 py-5 px-5 w-40 h-22 rounded-sm leading-8"> <span className="text-primary"><Blinds /></span>  Curtain</div>
+        <div className="card block bg-gray-200 py-5 px-5 w-40 h-22 rounded-sm leading-8"> <span className="text-primary"><Blinds /></span>  Curtain</div>
+      </div>
       <Button to="/step6-concerns" label="Continue" fixed />
     </div>
   );
